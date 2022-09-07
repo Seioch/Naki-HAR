5eb7a4c9bfa69adaa835eb0752b79d8dbe983d7b
Seioch <lightbluespectre@gmail.com>
2022-09-07 14:09:16 -0700

    More progress done, almost to feature parity with Kurin:

--- Source/Naki-HAR/Naki-HAR/Comps/compNakiWeapons.cs
+++ Source/Naki-HAR/Naki-HAR/Comps/compNakiWeapons.cs
@@ -9,40 +9,10 @@ using UnityEngine;
 
 namespace Naki_HAR.Comps
 {
-    public class compNakiWeapons : CompProperties
+    public class compNakiWeapons : ThingComp
     {
         public int maximumShots = 100;
-
         public int currentShots = 0;
-    }
-
-    public override IEnumerable<StatDrawEntry> SpecialDisplayStats(StatRequest req)
-    {
-        foreach (StatDrawEntry statDrawEntry in base.SpecialDisplayStats(req))
-        {
-            yield return statDrawEntry;
-        }
-        IEnumerator<StatDrawEntry> enumerator = null;
-        if (!req.HasThing)
-        {
-            yield return new StatDrawEntry(StatCategoryDefOf.Apparel, "Stat_Thing_ReloadMaxCharges_Name".Translate(this.ChargeNounArgument), this.maxCharges.ToString(), "Stat_Thing_ReloadMaxCharges_Desc".Translate(this.ChargeNounArgument), 2749, null, null, false);
-        }
-        if (this.ammoDef != null)
-        {
-            if (this.ammoCountToRefill != 0)
-            {
-                yield return new StatDrawEntry(StatCategoryDefOf.Apparel, "Stat_Thing_ReloadRefill_Name".Translate(this.ChargeNounArgument), string.Format("{0} {1}", this.ammoCountToRefill, this.ammoDef.label), "Stat_Thing_ReloadRefill_Desc".Translate(this.ChargeNounArgument), 2749, null, null, false);
-            }
-            else
-            {
-                yield return new StatDrawEntry(StatCategoryDefOf.Apparel, "Stat_Thing_ReloadPerCharge_Name".Translate(this.ChargeNounArgument), string.Format("{0} {1}", this.ammoCountPerCharge, this.ammoDef.label), "Stat_Thing_ReloadPerCharge_Desc".Translate(this.ChargeNounArgument), 2749, null, null, false);
-            }
-        }
-        if (this.destroyOnEmpty)
-        {
-            yield return new StatDrawEntry(StatCategoryDefOf.Apparel, "Stat_Thing_ReloadDestroyOnEmpty_Name".Translate(this.ChargeNounArgument), "Yes".Translate(), "Stat_Thing_ReloadDestroyOnEmpty_Desc".Translate(this.ChargeNounArgument), 2749, null, null, false);
-        }
-        yield break;
-        yield break;
+        public bool destroyOnEmpty = true;
     }
 }
---
+++ Source/Naki-HAR/Naki-HAR/Doers/IngestionOutcomeDoer_GiveHediffCheckRace.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using Naki_HAR.Helpers;
+
+namespace Naki_HAR.Doers
+{
+    class IngestionOutcomeDoer_GiveHediffCheckRace : IngestionOutcomeDoer
+    {
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+        {
+            if (!pawn.IsNaki()) // Only poison pawns if they are not Naki
+            {
+                Hediff hediff = HediffMaker.MakeHediff(this.hediffDef, pawn, null);
+                float num;
+                if (this.severity > 0f)
+                {
+                    num = this.severity;
+                }
+                else
+                {
+                    num = this.hediffDef.initialSeverity;
+                }
+                if (this.divideByBodySize)
+                {
+                    num /= pawn.BodySize;
+                }
+                // AddictionUtility.ModifyChemicalEffectForToleranceAndBodySize(pawn, this.toleranceChemical, ref num);
+                hediff.Severity = num;
+                pawn.health.AddHediff(hediff, null, null, null);
+            }
+        }
+
+        // Token: 0x060040B1 RID: 16561 RVA: 0x00161D32 File Offset: 0x0015FF32
+        public override IEnumerable<StatDrawEntry> SpecialDisplayStats(ThingDef parentDef)
+        {
+            if (parentDef.IsDrug && this.chance >= 1f)
+            {
+                foreach (StatDrawEntry statDrawEntry in this.hediffDef.SpecialDisplayStats(StatRequest.ForEmpty()))
+                {
+                    yield return statDrawEntry;
+                }
+                IEnumerator<StatDrawEntry> enumerator = null;
+            }
+            yield break;
+        }
+
+        // Token: 0x04002343 RID: 9027
+        public HediffDef hediffDef;
+
+        // Token: 0x04002344 RID: 9028
+        public float severity = -1f;
+
+        // Token: 0x04002345 RID: 9029
+        public ChemicalDef toleranceChemical;
+
+        // Token: 0x04002346 RID: 9030
+        private bool divideByBodySize;
+    }
+}
